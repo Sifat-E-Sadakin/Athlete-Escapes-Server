d1379961aa6f2679be0941c15b666d80c8e66a1d
Sifat-E-Sadakin <sifatesadakin10@gmail.comm>
2023-06-07 16:18:13 +0600

    saving user in db

--- index.js
+++ index.js
@@ -1,6 +1,54 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+require('dotenv').config()
 const port = process.env.PORT || 3000 
+
+app.use(cors())
+app.use(express.json())
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_U}:${process.env.DB_P}@cluster0.rohhp7w.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+
+
+    let userCollection = client.db('Athlete_Escapes').collection('users');
+
+    app.post('/users', async (req, res)=>{
+      let user = req.body;
+      let query = {email : user.email};
+      let oldUser = await userCollection.findOne(query);
+      if(oldUser){
+        return
+      }
+      let result = await userCollection.insertOne(user);
+      res.send(result);
+      
+    })
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
