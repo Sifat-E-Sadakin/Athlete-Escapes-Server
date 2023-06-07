cf9483d93cc28d7c1bac66616c0b8d6c466652e3
Sifat-E-Sadakin <sifatesadakin10@gmail.comm>
2023-06-07 19:13:18 +0600

    verify admin

--- index.js
+++ index.js
@@ -2,10 +2,27 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 3000 
 
 app.use(cors())
 app.use(express.json())
+
+let verifyJWT = (req, res, next)=>{
+  let authorization = req.headers.authorization;
+  if (!authorization){
+    return res.status(401).send({error: true, message : "unauthorized access" });
+  }
+  let token = authorization.split(' ')[1];
+
+  jwt.verify(token, process.env.WEB_TOKEN, (err, decoded)=>{
+    if (err){
+      return res.status(403).send({error: true, message : "unauthorized access" });
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -26,6 +43,39 @@ async function run() {
 
     let userCollection = client.db('Athlete_Escapes').collection('users');
 
+    
+    // Verify APIS
+
+    let verifyAdmin = async (req, res, next)=>{
+      let email = req.decoded.email
+      // console.log(email);
+      let query = { email : email}
+      let user = await userCollection.findOne(query);
+      if(user?.role != 'admin'){
+        return res.status(403).send({err: true, msg: 'forbidden'})
+      }
+      next();
+    }
+
+    app.get('/isAdmin/:email', async (req, res)=>{
+      let mail = req.params.email;
+      let filter = {email : mail}
+      let user = await userCollection.findOne(filter);
+      let result = { isAdmin : user?.role == 'admin'}
+      res.send(result);
+    })
+    
+    //JWT APIS
+
+
+
+    app.post('/jwt', (req, res)=>{
+      // console.log('hit jwt');
+      let user = req.body;
+      let token = jwt.sign(user, process.env.WEB_TOKEN, {expiresIn: '1h'} )
+      res.send({token})
+
+    })
 
     // Users APIS
 
@@ -41,7 +91,7 @@ async function run() {
       
     })
 
-    app.get('/users', async (req, res)=>{
+    app.get('/users', verifyJWT,verifyAdmin, async (req, res)=>{
       let result = await userCollection.find().toArray();
       res.send(result)
     })
@@ -84,6 +134,12 @@ async function run() {
       let result = await userCollection.updateOne(filter, updateRole);
       res.send(result);
 
+    })
+
+    app.get('/instructors', async (req, res)=>{
+        let filter = { role : 'instructor'}
+        let result = await userCollection.find(filter).toArray()
+        res.send(result)
     })
 
 
